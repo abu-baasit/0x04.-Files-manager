82c194561a74febbf2105b739e5333e11e5f03ee
abu-baasit <saheedegunsola@gnail.com>
2024-12-11 12:21:02 -0800

    ALX managing file

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,32 +1,67 @@
-import redisClient from '../utils/redis';
-import dbClient from '../utils/db';
+import dbClient from '../../utils/db';
 
-class AppController {
-  /**
-   * should return if Redis is alive and if the DB is alive too
-   * by using the 2 utils created previously:
-   * { "redis": true, "db": true } with a status code 200
-   */
-  static getStatus(request, response) {
-    const status = {
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
-    };
-    response.status(200).send(status);
-  }
+describe('+ AppController', () => {
+  before(function (done) {
+    this.timeout(10000);
+    Promise.all([dbClient.usersCollection(), dbClient.filesCollection()])
+      .then(([usersCollection, filesCollection]) => {
+        Promise.all([usersCollection.deleteMany({}), filesCollection.deleteMany({})])
+          .then(() => done())
+          .catch((deleteErr) => done(deleteErr));
+      }).catch((connectErr) => done(connectErr));
+  });
 
-  /**
-   * should return the number of users and files in DB:
-   * { "users": 12, "files": 1231 }
-   *  with a status code 200
-   */
-  static async getStats(request, response) {
-    const stats = {
-      users: await dbClient.nbUsers(),
-      files: await dbClient.nbFiles(),
-    };
-    response.status(200).send(stats);
-  }
-}
+  describe('+ GET: /status', () => {
+    it('+ Services are online', function (done) {
+      request.get('/status')
+        .expect(200)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          expect(res.body).to.deep.eql({ redis: true, db: true });
+          done();
+        });
+    });
+  });
 
-export default AppController;
+  describe('+ GET: /stats', () => {
+    it('+ Correct statistics about db collections', function (done) {
+      request.get('/stats')
+        .expect(200)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          expect(res.body).to.deep.eql({ users: 0, files: 0 });
+          done();
+        });
+    });
+
+    it('+ Correct statistics about db collections [alt]', function (done) {
+      this.timeout(10000);
+      Promise.all([dbClient.usersCollection(), dbClient.filesCollection()])
+        .then(([usersCollection, filesCollection]) => {
+          Promise.all([
+            usersCollection.insertMany([{ email: 'john@mail.com' }]),
+            filesCollection.insertMany([
+              { name: 'foo.txt', type: 'file'},
+              {name: 'pic.png', type: 'image' },
+            ])
+          ])
+            .then(() => {
+              request.get('/stats')
+                .expect(200)
+                .end((err, res) => {
+                  if (err) {
+                    return done(err);
+                  }
+                  expect(res.body).to.deep.eql({ users: 1, files: 2 });
+                  done();
+                });
+            })
+            .catch((deleteErr) => done(deleteErr));
+        }).catch((connectErr) => done(connectErr));
+    });
+  });
+});
--- utils/db.js
+++ utils/db.js
@@ -1,55 +1,25 @@
-import { MongoClient } from 'mongodb';
+import dbClient from '../../utils/db';
 
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || 27017;
-const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
-const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+describe('+ DBClient utility', () => {
+  before(function (done) {
+    this.timeout(10000);
+    Promise.all([dbClient.usersCollection(), dbClient.filesCollection()])
+      .then(([usersCollection, filesCollection]) => {
+        Promise.all([usersCollection.deleteMany({}), filesCollection.deleteMany({})])
+          .then(() => done())
+          .catch((deleteErr) => done(deleteErr));
+      }).catch((connectErr) => done(connectErr));
+  });
 
-/**
- * Class for performing operations with Mongo service
- */
-class DBClient {
-  constructor() {
-    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-      if (!err) {
-        // console.log('Connected successfully to server');
-        this.db = client.db(DB_DATABASE);
-        this.usersCollection = this.db.collection('users');
-        this.filesCollection = this.db.collection('files');
-      } else {
-        console.log(err.message);
-        this.db = false;
-      }
-    });
-  }
+  it('+ Client is alive', () => {
+    expect(dbClient.isAlive()).to.equal(true);
+  });
 
-  /**
-   * Checks if connection to Redis is Alive
-   * @return {boolean} true if connection alive or false if not
-   */
-  isAlive() {
-    return !!this.db;
-  }
+  it('+ nbUsers returns the correct value', async () => {
+    expect(await dbClient.nbUsers()).to.equal(0);
+  });
 
-  /**
-   * Returns the number of documents in the collection users
-   * @return {number} amount of users
-   */
-  async nbUsers() {
-    const numberOfUsers = this.usersCollection.countDocuments();
-    return numberOfUsers;
-  }
-
-  /**
-   * Returns the number of documents in the collection files
-   * @return {number} amount of files
-   */
-  async nbFiles() {
-    const numberOfFiles = this.filesCollection.countDocuments();
-    return numberOfFiles;
-  }
-}
-
-const dbClient = new DBClient();
-
-export default dbClient;
+  it('+ nbFiles returns the correct value', async () => {
+    expect(await dbClient.nbFiles()).to.equal(0);
+  });
+});
